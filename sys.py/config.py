63c78a5f152471a99e34491c6784db614ac928e9
cuu <dexterkidd@gmail.com>
2018-07-02 16:48:27 +0800

    add detail on power mode

--- sys.py/config.py
+++ sys.py/config.py
@@ -16,8 +16,10 @@ VERSION="stable 1.0"
 
 SKIN="default"
 
+## three timer values in seconds: dim screen, close screen,PowerOff
+## zero means no action
 PowerLevels = {}
-PowerLevels["supersaving"] = [10,10,10]
+PowerLevels["supersaving"] = [10,30,100]
 PowerLevels["powersaving"] = [40,120,300]
 PowerLevels["balance_saving"] = [40,0,0]
 
