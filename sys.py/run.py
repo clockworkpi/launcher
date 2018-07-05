e029db63e63bb9f67df7ba58e492560f63aa0c19
cuu <dexterkidd@gmail.com>
2018-07-05 13:19:04 +0800

    ...

--- sys.py/run.py
+++ sys.py/run.py
@@ -247,7 +247,6 @@ def event_process(event,main_screen):
 
         if event.type == POWEROPT:
             everytime_keydown = time.time()
-            RestoreLastBackLightBrightness(main_screen)
             
             return
         if event.type == pygame.KEYUP:
