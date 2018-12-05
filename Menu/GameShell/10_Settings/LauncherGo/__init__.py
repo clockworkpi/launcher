dd3b95b3f5427c711f0760d467f6d9d3c76f4164
cuu <dexterkidd@gmail.com>
2018-12-05 20:17:22 +0800

    switch to launchego

---
+++ Menu/GameShell/10_Settings/LauncherGo/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*- 
+import os
+import pygame
+#import math
+#mport subprocess
+import glob
+#from beeprint import pp
+from libs.roundrects import aa_round_rect
+
+## local UI import
+from UI.lang_manager import MyLangManager
+
+class APIOBJ(object):
+
+    _Page = None
+    
+    def __init__(self):
+        pass
+    def Init(self,main_screen):
+        pass
+    def API(self,main_screen):
+        if main_screen !=None:
+            main_screen._MsgBox.SetText("Rebooting to LauncherGo")
+            main_screen._MsgBox.Draw()
+            main_screen.SwapAndShow()
+            pygame.time.delay(300)
+            os.system("sed -i s/launcher/launchergo/g ~/.bashrc" )
+            os.system("sudo reboot")
+
+OBJ = APIOBJ()
+def Init(main_screen):    
+    OBJ.Init(main_screen)
+def API(main_screen):
+    OBJ.API(main_screen)
