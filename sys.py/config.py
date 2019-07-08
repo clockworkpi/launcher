4af70ee8bcebfc480398df257ee32a5e08071857
Noah <noahbehtel@gmail.com>
2019-07-07 23:07:35 -0500

    once more

--- sys.py/config.py
+++ sys.py/config.py
@@ -1,67 +1,70 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import os
 import platform
-from UI.util_funcs   import FileExists,ArmSystem
+from UI.util_funcs import FileExists, ArmSystem
 
-CurKeySet = "GameShell" ## >>>    PC or GameShell   <<<
+CurKeySet = "GameShell"  # >>>    PC or GameShell   <<<
 
 DontLeave = False
 
 BackLight = "/proc/driver/backlight"
-Battery   = "/sys/class/power_supply/axp20x-battery/uevent"
+Battery = "/sys/class/power_supply/axp20x-battery/uevent"
 
 
 MPD_socket = "/tmp/mpd.socket"
 
-UPDATE_URL="https://raw.githubusercontent.com/clockworkpi/CPI/master/launcher_ver0.4.json"
 
-VERSION="stable 1.25"
+UPDATE_URL = "https://raw.githubusercontent.com/clockworkpi/CPI/master/launcher_ver0.4.json"
 
-SKIN=None
+VERSION = "stable 1.25"
 
-ButtonsLayout="xbox"
+SKIN = "../skin/default"
 
-## three timer values in seconds: dim screen, close screen,PowerOff
-## zero means no action
+# three timer values in seconds: dim screen, close screen,PowerOff
+# zero means no action
+ButtonsLayout = "xbox"
+
+# three timer values in seconds: dim screen, close screen,PowerOff
+# zero means no action
 PowerLevels = {}
-PowerLevels["supersaving"] = [10,30,120]
-PowerLevels["powersaving"] = [40,120,300]
-PowerLevels["server"]      = [40,120,0]
-PowerLevels["balance_saving"] = [40,0,0]
+PowerLevels["supersaving"] = [10, 30, 120]
+PowerLevels["powersaving"] = [40, 120, 300]
+PowerLevels["server"] = [40, 120, 0]
+PowerLevels["balance_saving"] = [40, 0, 0]
 
 PowerLevel = "balance_saving"
+Wallpaper = ''
+
 
 def PreparationInAdv():
-    global SKIN,ButtonsLayout
+    global SKIN, ButtonsLayout
     global PowerLevel
-    
-    if SKIN != None:
-        return
+    global Wallpaper
 
-    SKIN= "../skin/default"
-    
+    SKIN = "../skin/default"
+
     if FileExists("%s/.gameshell_skin" % os.path.expanduser('~')) == True:
-        with open("%s/.gameshell_skin" % os.path.expanduser('~'),"r") as f:
-          gameshell_skin = f.read()
-        
+        with open("%s/.gameshell_skin" % os.path.expanduser('~'), "r") as f:
+            gameshell_skin = f.read()
+
         gameshell_skin = gameshell_skin.strip()
-        SKIN= gameshell_skin
-        
+        SKIN = gameshell_skin
+
     if FileExists(".buttonslayout") == True:
         with open(".buttonslayout") as f:
             btnlayout = f.read()
-        
+
         btnlayout = btnlayout.strip()
         ButtonsLayout = btnlayout
         if ButtonsLayout != "xbox" and ButtonsLayout != "snes":
             ButtonsLayout = "xbox"
-        
+
     if FileExists(".powerlevel") == False:
         os.system("touch .powerlevel")
-    
-    with open(".powerlevel","r") as f:
+
+    with open(".powerlevel", "r") as f:
         powerlevel = f.read()
-    
+
     powerlevel = powerlevel.strip()
     if powerlevel != "":
         PowerLevel = powerlevel
@@ -72,6 +75,29 @@ def PreparationInAdv():
     else:
         ArmSystem("sudo iw wlan0 set power_save off >/dev/null")
 
-PreparationInAdv()
-##sys.py/.powerlevel
+    if FileExists(".wallpaper") == False:
+        os.system("touch .wallpaper")
 
+    with open(".wallpaper", "r") as w:
+        wallpaper = w.read()
+
+    if wallpaper != "":
+        Wallpaper = wallpaper
+
+    if SKIN != None:
+        return
+
+    SKIN = "../skin/default"
+
+    if FileExists("%s/.gameshell_skin" % os.path.expanduser('~')) == True:
+        with open("%s/.gameshell_skin" % os.path.expanduser('~'), "r") as f:
+            gameshell_skin = f.read()
+
+        gameshell_skin = gameshell_skin.strip()
+        SKIN = gameshell_skin
+
+
+PreparationInAdv()
+# sys.py/.powerlevel
+
+# sys.py/.powerlevel
