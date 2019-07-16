4f7fcc6433fb3dfab9572d4d19b4cb24d481cb2e
cuu <dexterkidd@gmail.com>
2019-07-16 20:45:13 +0800

    update about

--- sys.py/UI/icon_pool.py
+++ sys.py/UI/icon_pool.py
@@ -33,7 +33,20 @@ class IconPool(object):
     def Height(self,keyname):
         if keyname in self._Sizes:
             return self._Sizes[keyname][1]
-
+            
+    def GiveIconSurface(self,imgname): ## imgname is the png file name without .png
+        if imgname in self._Icons: 
+            return self._Icons[imgname]
+        else:
+            icon_file = MySkinManager.GiveIcon("gameshell/icons/"+imgname+".png")
+            if os.path.isfile(icon_file):
+                keyname = imgname
+                self._Icons[keyname] = pygame.image.load(icon_file).convert_alpha()
+                self._Sizes[keyname] = self._Icons[keyname].get_size()
+                return self._Icons[keyname]
+        
+        return None # this will cause panic,if not found both in theme and default skin folder
+        
 ##global Handler
 MyIconPool = None
 
@@ -43,5 +56,4 @@ def InitMyIconPool():
         MyIconPool = IconPool()
 
 InitMyIconPool()
-
 
