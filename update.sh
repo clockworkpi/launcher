dc4f61ee921781829f5648bf41ad84ff17c2e7d4
cuu <dexterkidd@gmail.com>
2019-08-07 21:40:50 +0800

    add patch for mame2003plus

--- update.sh
+++ update.sh
@@ -6,6 +6,8 @@ git pull
 git reset --hard $1
 git submodule init
 git submodule update
+cd ~/apps/Menu && git pull origin gcores && cd -
+
 feh --bg-center /home/cpi/launcher/sys.py/gameshell/wallpaper/loading.png 
 ./load.sh
 
