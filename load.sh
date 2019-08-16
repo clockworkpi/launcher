7917aaaddc1269a6580ea1dd69b93676a32287cb
cpi <cpi@clockworkpi>
2019-08-16 14:59:27 +0800

    add Menu update before launcher

--- load.sh
+++ load.sh
@@ -4,6 +4,15 @@ BASENAME=`dirname "$0"`
 
 cd $BASENAME/sys.py
 
+if [ ! -f ~/.apps_menu_updated ]
+then
+cd ~/apps/Menu && git pull origin gcores
+if [ $? -eq 0 ]; then
+touch ~/.apps_menu_updated
+fi
+cd -
+fi
+
 python run.py
 
 
--- update.sh
+++ update.sh
@@ -6,7 +6,8 @@ git pull
 git reset --hard $1
 git submodule init
 git submodule update
-cd ~/apps/Menu && git pull origin gcores && cd -
+#cd ~/apps/Menu && git pull origin gcores && cd -
+rm ~/.apps_menu_updated
 
 feh --bg-center /home/cpi/launcher/sys.py/gameshell/wallpaper/loading.png 
 ./load.sh
